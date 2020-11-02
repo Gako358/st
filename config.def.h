8e2f44ab7eb2393431811425f3e1a08a7d92981c
Gako358 <gako358@outlook.com>
2020-11-02 18:31:07 +0100

    font size st

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansMono Nerd Font:pixelsize=15:antialias=true:autohint=true";
+static char *font = "DejaVuSansMono Nerd Font:pixelsize=13:antialias=true:autohint=true";
 static int borderpx = 1;
 
 /*
