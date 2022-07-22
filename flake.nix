99d1cceb5555a020d41de5ed2e0c673f09a5d47d
Gako358 <gako358@outlook.com>
2022-07-22 15:06:48 +0200

    update

--- flake.nix
+++ flake.nix
@@ -3,38 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs }: 
       let
-        pkgs = import nixpkgs {
-	  inherit system;
-	  overlays = [
-	    (final: prev: {
-	      st_mx = prev.st.overrideAttrs (oldAttrs: rec {
-	        version = "main";
-		src = ./.;
-		buildInputs = oldAttrs.buildInputs ++ [
-		  prev.harfbuzz.dev
-		];
-	      });
-	    })
-          ];
+        system = "x86_64-linux";
+	overlay = final: prev: {
+	  st_mx = prev.st.overrideAttrs (oldAttrs: rec {
+	    version = "main";
+	    src = ./.;
+	    buildInputs = oldAttrs.buildInputs ++ [
+	      prev.harfbuzz.dev
+	    ];
+	  });
 	};
 	in
 	rec {
-	  apps = {
-	    st = {
-	      type = "app";
-	      program = "${defaultPackage}/bin/st";
-	    };
-	  };
-
-	  package.st_mx = pkgs.st_mx;
-	  defaultApp = apps.st;
-	  defaultPackage = pkgs.st_mx;
-	}
-    );
+	  overlays.default = overlay;
+	  checks.${system}.build = (
+            import nixpkgs {
+	      inherit system;
+	      overlays = [ overlay ];
+	    }
+	  ).st;
+	};
 }
